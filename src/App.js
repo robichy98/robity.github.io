e87701e903b62ebdcd503a5834c0d7e45df5873c
robichy98 <63616953+robichy98@users.noreply.github.com>
2021-09-26 16:06:33 -0400

    final commit, I will become a React pro!

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/robichy98/robity.github.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check Me Out!
         </a>
       </header>
     </div>
